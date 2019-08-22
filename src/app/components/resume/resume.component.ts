b0939b79e63306eb08097f81374b605d26dc90b3
Adam Callanan <a.callanan@sbcglobal.net>
2019-08-22 18:59:29 -0500

    added icons to resume

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { faMicrophone, faFilm, faMusic, faPrint, faCamera } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-resume",
@@ -6,6 +7,11 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./resume.component.scss"]
 })
 export class ResumeComponent implements OnInit {
+  public faMicrophone = faMicrophone;
+  public faFilm = faFilm;
+  public faMusic = faMusic;
+  public faCamera = faCamera;
+  public faPrint = faPrint;
 
   constructor() { }
 
