12e2f86f5399538cac5508122c637ae5d614331c
Adam Callanan <a.callanan@sbcglobal.net>
2019-08-21 23:07:07 -0500

    added old footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faTwitter, faYoutube, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: "app-footer",
@@ -6,7 +8,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./footer.component.scss"]
 })
 export class FooterComponent implements OnInit {
-  year = new Date().getFullYear();
+  public year = new Date().getFullYear();
+
+  public faEnvelope = faEnvelope;
+  public faFacebook = faFacebook;
+  public faTwitter = faTwitter;
+  public faYoutube = faYoutube;
+  public faInstagram = faInstagram;
 
   constructor() { }
 
