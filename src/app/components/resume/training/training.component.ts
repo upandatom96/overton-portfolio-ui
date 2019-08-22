5460fc912da55a2684787b20fb3d3ecac5ff50f7
Adam Callanan <a.callanan@sbcglobal.net>
2019-08-21 23:45:56 -0500

    added training component

--- src/app/components/resume/training/training.component.ts
+++ src/app/components/resume/training/training.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Training } from 'src/app/models/Training.model';
 
 @Component({
   selector: 'app-training',
@@ -6,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training.component.scss']
 })
 export class TrainingComponent implements OnInit {
+  training: Training[] = [
+    {
+      title: "BFA Musical Theatre Degree",
+      institution: "Drake University"
+    },
+    {
+      title: "Performance Apprentice",
+      institution: "Red Branch Theatre Company"
+    },
+    {
+      title: "Acting",
+      instructors: [
+        "Brian McManamon",
+        "Karla Kash",
+        "Michael Cadman",
+        "Jason Bohon",
+        "Forrest McClendon"
+      ]
+    },
+    {
+      title: "Dance",
+      instructors: ["Paula McArthur", "David Dorfman", "Derrick Yanfork"]
+    }
+  ];
 
   constructor() { }
 
