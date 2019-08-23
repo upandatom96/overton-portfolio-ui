78bd5b1845ed0727cb88c2be1e506bacccb03d74
Adam Callanan <a.callanan@sbcglobal.net>
2019-08-22 19:31:01 -0500

    moved production stills into static data

--- src/app/components/media/production-stills/production-stills.component.ts
+++ src/app/components/media/production-stills/production-stills.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./production-stills.component.scss']
 })
 export class ProductionStillsComponent implements OnInit {
+  public productionStillList: string[] = [
+    "https://unsplash.it/600.jpg?image=252",
+    "https://unsplash.it/600.jpg?image=253",
+    "https://unsplash.it/600.jpg?image=254",
+    "https://unsplash.it/600.jpg?image=255",
+    "https://unsplash.it/600.jpg?image=256",
+    "https://unsplash.it/600.jpg?image=257",
+    "https://unsplash.it/600.jpg?image=258",
+    "https://unsplash.it/600.jpg?image=259",
+  ];
 
   constructor() { }
 
