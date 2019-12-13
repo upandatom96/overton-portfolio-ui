ca8718a00582798896e85bf8e6ba93f21cc190ff
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-12 21:52:15 -0600

    added new endpoints into show service

--- src/app/services/show.service.ts
+++ src/app/services/show.service.ts
@@ -23,13 +23,28 @@ export class ShowService {
         });
   }
 
-  private retrieveShows() {
+  private retrieveShows(): Observable<ShowResource> {
     const url = RestUtilities.buildUrl("show");
     return this.http.get(url) as Observable<ShowResource>;
   }
 
-  public createShow(show: Show) {
+  private retrieveShowById(showId: string): Observable<Show> {
+    const url = RestUtilities.buildUrl(`show/${showId}`);
+    return this.http.get(url) as Observable<Show>;
+  }
+
+  public createShow(show: Show): Observable<any> {
     const url = RestUtilities.buildUrl("show");
-    return this.http.post(url, show) as Observable<ShowResource>;
+    return this.http.post(url, show) as Observable<any>;
+  }
+
+  public deleteShow(showId: string): Observable<any> {
+    const url = RestUtilities.buildUrl(`show/${showId}`);
+    return this.http.delete(url) as Observable<any>;
+  }
+
+  public updateShow(show: Show): Observable<any> {
+    const url = RestUtilities.buildUrl("show");
+    return this.http.put(url, show) as Observable<any>;
   }
 }
