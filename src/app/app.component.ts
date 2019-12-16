c91742d6bb2dcd92cd1612f65372730b9a2e30ae
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-15 21:59:49 -0600

    load text via app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { TextService } from './services/text.service';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "overton-portfolio";
   faLock = faLock;
+
+  constructor(
+    private textService: TextService,
+  ) { }
+
+  public ngOnInit() {
+    this.textService.loadText();
+  }
 }
