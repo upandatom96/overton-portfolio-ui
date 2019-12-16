c3d231774d62ae267da30af63a0faadf70489935
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-15 21:58:34 -0600

    setting up text service

---
+++ src/app/services/text.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RestHelperService } from './rest-helper.service';
+import { Observable } from 'rxjs';
+import { BooleanUtilities } from '../utilities/boolean.utilities';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TextService {
+  private textResource;
+
+  public get textReady(): boolean {
+    return !BooleanUtilities.isNullOrUndefined(this.textResource);
+  }
+
+  constructor(
+    private http: HttpClient,
+    private restHelperService: RestHelperService,
+  ) { }
+
+  public loadText(): void {
+    this.retrieveText()
+      .subscribe((res) => this.textResource = res,
+        (error) => {
+          console.log("send email failed");
+        });
+  }
+
+  private retrieveText(): Observable<any> {
+    const url = this.restHelperService.buildRestUrl("text");
+    return this.http.get(url, this.restHelperService.headersWithAuth) as Observable<any>;
+  }
+}
