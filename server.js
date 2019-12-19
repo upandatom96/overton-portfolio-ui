44827e9c23c6755f7ff8c836aa0da09b3dea6228
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-18 21:32:34 -0600

    try again

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// server.js
 
+const path = require('path');
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/overton-portfolio'));
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/dist/overton-portfolio/index.html'));
-});
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
 
-// Start the app by listening on the default Heroku port
+// Instruct the app
+// to use the forceSSL
+// middleware
+
+app.use(forceSSL());
+
+// run the app by serving the static files
+// in the dist directory
+
+app.use(express.static(__dirname + '/dist'));
+
+// start the app by listening on the default heroku port
+
 app.listen(process.env.PORT || 8080);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
