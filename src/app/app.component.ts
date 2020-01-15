d86612472be0b223d70ae16037825f8b8cfaf085
Adam Callanan <a.callanan@sbcglobal.net>
2020-01-14 20:31:09 -0600

    remove test code from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import { TextService } from "./services/text.service";
-import { RestHelperService } from './services/rest-helper.service';
 
 @Component({
   selector: "app-root",
@@ -15,12 +14,9 @@ export class AppComponent implements OnInit {
 
   constructor(
     private textService: TextService,
-    private restHelper: RestHelperService,
   ) { }
 
   public ngOnInit() {
-    const envUrl = this.restHelper.baseServiceUrl;
-    console.log(envUrl);
     this.textService.loadText();
   }
 }
