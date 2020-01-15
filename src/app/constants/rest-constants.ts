59dd3644dfc29f691de57d2d26aa5ba5856f9a3f
Adam Callanan <a.callanan@sbcglobal.net>
2020-01-14 18:15:33 -0600

    added prod and test url

--- src/app/constants/rest-constants.ts
+++ src/app/constants/rest-constants.ts
@@ -1,1 +1,3 @@
 export const BASE_SERVICES_URL = "https://overton-portfolio.herokuapp.com/";
+export const PROD_URL = "https://overton-portfolio-services.herokuapp.com/";
+export const TEST_URL = "https://overton-portfolio.herokuapp.com/";
